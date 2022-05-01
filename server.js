323a04a45955bfaf0e74e38f96cd18914a4c5ec4
Olivia Bloway <blowayol@msu.edu>
2022-05-01 09:00:06 -0400

    set up local host port, that server will be listening on

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+const { response } = require("express")
+const { request } = require("express")
 const express = require("express")
-
 const app = express()
+const port = 8000
 
 app.get('/', (request, response)=>{
 response.sendFile(__dirname + '/index.html')
 })
 
+app.listen(port, (request, response)=>{
+    console.log(`the server is now running on ${port}`)
+})
