347c89f9627bf746441b4521381b122c707b2b9b
Olivia Bloway <blowayol@msu.edu>
2022-06-14 15:54:31 -0400

    updated everything

--- server.js
+++ server.js
@@ -2,11 +2,13 @@
 const express = require("express")
 const app = express()
 const cors = require('cors')
-const { response } = require("express")
 const PORT = 8000
+const MongoClient = require('mongodb').MongoClient
+const connectionString = "mongodb+srv://livBridg1:Bridgerton1@clusterbridgerton.hfgedn9.mongodb.net/?retryWrites=true&w=majority"
 app.use(cors())
+app.use(express.json())
 
-let characters = {
+/*let characters = {
         "daphne bridgerton" : {
             "first appearance": "season 1 ep 1",
              "titles"         : "Dutches of Hastings",
@@ -63,36 +65,62 @@ let characters = {
             "first appearance"  : "season 1 ep 1",
             "title"             : "none",
             "portrayed by"      : "Nicola Coughlan"
-        },
+        }
+
+}
+*/
 
 
+MongoClient.connect(connectionString)
+    .then(client =>{
+    //if (err) return console.error(err)
+    console.log('Connected to Database')
+    const db = client.db('bridgerton1')
+    const characterCollection = db.collection('bridgertonCharacters2')
+    
+
+    app.get('/', (request, response)=>{
+    response.sendFile(__dirname + '/index.html')
+    })
+
+    //app.get('/api', (request,response)=>{
+       // response.json()
+    
+    //})
+
+    app.get('/api/:name', (request, response)=>{
+        const charactersName = request.params.name.toLowerCase()
+        characterCollection.find({name:charactersName}).toArray()
+            .then(results =>{
+                console.log(results)
+                response.json(results[0])
+             })
         
-     
+            .catch(error => console.error(error))
+        
+    })
+    
+   
+    
+ })
+ .catch(error => console.error(error))
 
-
-
-    }
-
-app.get('//', (request, response)=>{
-response.sendFile(__dirname + '/index.html')
-})
-
-app.get('/api', (request,response)=>{
-    response.json(characters)
-
-})
-
-app.get('/api/:name', (request, response)=>{
-    const characterName = request.params.name.toLowerCase()
-    if(characters[characterName]){
-        response.json(characters[characterName])
-    }else{
-        response.json(characters['unknown'])
-    }
-
-})
-
-
-app.listen(process.env.PORT || PORT ,(request, response)=>{
+ app.listen(process.env.PORT || PORT ,(request, response)=>{
     console.log(`the server is now running on ${PORT}`)
 })
+ //.catch(error => console.error(error)) 
+
+
+
+
+   
+    
+
+
+
+
+
+
+
+
+
